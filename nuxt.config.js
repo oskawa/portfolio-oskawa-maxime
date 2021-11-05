89c2bc43b04715f547b74f58f208cda011eeee72
Maxime Eloir <maxime.eloir@gmail.com>
2021-11-05 01:47:06 +0100

    Metadata

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
  
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
-  ssr: false,
+  ssr: true,
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
